9ccd0f952279bbb9aaf3d4da19067383b6efc6ff
user <user@LAPTOP-QOF44QE3>
2021-04-23 23:45:42 +0530

    by suraj555

--- Testing GitHub Demo2/src/Git_Demo2.java
+++ Testing GitHub Demo2/src/Git_Demo2.java
@@ -6,9 +6,17 @@ System.out.println("m1 method");
 }
 public void m2() {
 	System.out.println("m2 method");
+	m1();
 }
 	
 	public void m3() {
 	System.out.println("m3--- method");
+	m2();
+	
 }
+	public static void main(String[] args) {
+		System.out.println("main method");
+		Git_Demo2 g=new Git_Demo2();
+		g.m3();
+	}
 }
